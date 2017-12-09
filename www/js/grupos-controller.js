c89f85aac3731a98f62161b133e19a0b74edceea
Walter Osorio <Walter Osorio@INGDES28>
2017-12-09 15:49:09 -0500

    Gurdar grupo

--- www/js/grupos-controller.js
+++ www/js/grupos-controller.js
@@ -18,18 +18,22 @@ c.GruposController = function () {
     this.$gruposSalir = null;
     this.$gruposAddSalir = null;
     this.$pageSignIn = null;
+    this.$pageGrupos = null;
     this.$btnGuardarGrupos = null;
+    this.$nombreGrupo = null;
+    
 };
 c.GruposController.prototype.init = function () {
     //asociar las variables a los html
     this.$grupos = $("#grupos");
     this.$gruposAdd = $("#grupos-add");
     this.$gruposSalir = $("#grupos-salir", this.$grupos);
-    
+    this.$nombreGrupo =  $("#nombre-grupo", this.$gruposAdd);
     this.$btnGuardarGrupos = $("#btn-guardar-grupo", this.$gruposAdd);
     this.$gruposAddSalir = $("#gruposadd-salir", this.$gruposAdd);
-     
+    this.$pageGrupos="#grupos"; 
     this.$pageSignIn = "#page-signin";
+    
     //this.$btnCargarGrupos = $("#btn-cargar-dispositivos", this.$grupos);
 };
 
@@ -39,11 +43,41 @@ c.GruposController.prototype.cerrarSession = function () {
     $.mobile.navigate(me.$pageSignIn);
 };
 
-c.GruposController.prototype.guardarGrupo = function () {
-    alert("Ingreso");
-    var me = this;
-    c.Session.deleteInstance();
-    $.mobile.navigate(me.$pageSignIn);
+c.GruposController.prototype.guardarGrupo = function (nombreGrupo,correo) {
+    //alert("Ingreso");
+    var url = c.Settings.gruposUrl.replace("{correo}", usuario.correo);
+        
+    $.ajax({
+        url: url,
+        type: c.Settings.TYPE_GET,
+        dataType: c.Settings.DATA_TYPE_JSON,
+        contentType: c.Settings.APPLICATION_JSON,
+        success: function (resp) {
+            
+     
+            
+            
+            
+           
+            
+           var me = this;
+           $.mobile.navigate(me.$pageGrupos);
+
+          
+        },
+        error: function (e) {
+            var mensaje = message(e);
+            if (mensaje == null) {
+                console.log("error al cargar grupos.")
+            } else {
+                console.log(mensaje);
+            }
+        }
+    });
+    
+    
+    
+  
 };
 
 c.GruposController.prototype.cargarGrupos = function (usuario) {
